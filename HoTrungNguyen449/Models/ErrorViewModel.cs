91525e315236726c5d6f3f5de9ea3d09b7d9c2ac
axaxax312 <hotrungnguyen2001@gmail.com>
2023-01-03 10:16:08 +0700

    first commit

---
+++ HoTrungNguyen449/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace HoTrungNguyen449.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
